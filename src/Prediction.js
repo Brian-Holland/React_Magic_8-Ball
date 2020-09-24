0fd0e5a2b07097184837226f099cc07aaee0ff3f
Brian <brian.holland.pr@gmail.com>
2020-09-24 14:44:22 -0700

    added 8 stlying and mobile

--- src/Prediction.js
+++ src/Prediction.js
@@ -1,16 +1,20 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
 class Prediction extends Component {
-    render() {
-        return (
-            <div className="inner-ball">
-                <div className={`${this.props.fade ? "triangle" : ""}`}>
-                    <span hidden={!this.props.fade}>{this.props.result}</span>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className={`${this.props.result ? 'inner-ball answered' : 'inner-ball unanswered'}`}>
+				{this.props.result ? (
+					<div className={`${this.props.fade ? 'triangle' : ''}`}>
+						<span hidden={!this.props.fade}>{this.props.result}</span>
+					</div>
+				) : (
+					<span className="eight">8</span>
+				)}
+			</div>
+		);
+	}
 }
 
 export default Prediction;
